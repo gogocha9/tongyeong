c08e036c1d3fee547d02c9ad6d07833d2d290d81
YoungHyun Cha <gogocha9@naver.com>
2022-01-11 18:00:57 +0900

    commit

---
+++ vue/TongYeong.vue
@@ -0,0 +1,46 @@
+<template>
+   <div>
+       <div id="wrap">
+            <div :class="header">
+                <div :class="header-top">
+                    <h3 :class="logo"><a href="#">TongYeong</a></h3>
+                    <ul :class="header-gnb">
+                        <li><a href="#">홈</a></li>
+                        <li><a href="#">소개</a></li>
+                        <li><a href="#">문의</a></li>
+                        <li><a href="#">관리 시스템 가기</a></li>
+                    </ul>
+                </div>
+
+                <div :class="header-middle">
+                    <h3>통영예선은 귀하의 충실한 동반자입니다</h3>
+                    <a href="#">READ MORE</a>
+                </div>
+
+                <div :class="header-si">
+                    <ul>
+                        <li><a href="#">f</a></li>
+                        <li><a href="#">t</a></li>
+                        <li><a href="#">i</a></li>
+                        <li><a href="#">y</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+   </div>
+</template>
+
+<script>
+    
+</script>
+
+<style>
+    #wrap {
+        width: 100%;
+        height: 100%;
+    }
+
+    .header {
+        
+    }
+</style>
---
+++ vue/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import TongYeong from './TongYeong';
+
+new Vue(TongYeong).$mount('#root');
