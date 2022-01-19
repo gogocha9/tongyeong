0acf259d79ef3c19d603596c9e5ceaa844f9aac2
YoungHyun Cha <gogocha9@naver.com>
2022-01-19 10:03:16 +0900

    Header 완성

--- tongyeong/js/navigation.js
+++ tongyeong/js/navigation.js
@@ -5,9 +5,21 @@ let gnb = document.querySelector('#gnb');
 let navigation = document.querySelector('#navigation');
 let body = document.querySelector('body');
 
+window.onscroll = function() {
+    if(window.scrollY > 0) {
+        gnb.classList.add('down');
+    } else if (window.scrollY === 0) {
+        gnb.classList.remove('down');
+    }
+}
+
 menu_sm.onclick = function() {
     gnb.classList.toggle('active');
     menu_sm.classList.toggle('active');
     navigation.classList.toggle('active');
     body.classList.toggle('active');
 };
+
+gnb.scroll = function() {
+    
+}
