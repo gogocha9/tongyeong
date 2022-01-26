0d0a93fd5cdf6340e99895e482c0eb0523500fbb
YoungHyun Cha <gogocha9@naver.com>
2022-01-26 18:01:10 +0900

    메뉴바 구현

---
+++ tongyeong/js/intro.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let menu_sm = document.querySelector('#menu-sm');
+let gnb = document.querySelector('#gnb');
+let navigation = document.querySelector('#navigation');
+let body = document.querySelector('body');
+let toTop = document.querySelector('#toTop');
+
+window.onscroll = function() {
+    if(window.scrollY > 0) {
+        toTop.classList.add('down');
+        gnb.classList.add('down');
+    } else if (window.scrollY === 0) {
+        gnb.classList.remove('down');
+        toTop.classList.remove('down');
+    }
+}
+
+menu_sm.onclick = function() {
+    gnb.classList.toggle('active');
+    menu_sm.classList.toggle('active');
+    navigation.classList.toggle('active');
+    body.classList.toggle('active');
+};
--- tongyeong/js/navigation.js
+++ tongyeong/js/navigation.js
@@ -1,34 +1,19 @@
-"use strict";
-
-let menu_sm = document.querySelector('#menu-sm');
-let gnb = document.querySelector('#gnb');
-let navigation = document.querySelector('#navigation');
-let body = document.querySelector('body');
-let toTop = document.querySelector('#toTop');
-let partners_tab_wrap = document.querySelectorAll('#partners_tab_wrap li button');
-
-window.onscroll = function() {
-    if(window.scrollY > 0) {
-        toTop.classList.add('down');
-        gnb.classList.add('down');
-    } else if (window.scrollY === 0) {
-        gnb.classList.remove('down');
-        toTop.classList.remove('down');
-    }
-}
-
-menu_sm.onclick = function() {
-    gnb.classList.toggle('active');
-    menu_sm.classList.toggle('active');
-    navigation.classList.toggle('active');
-    body.classList.toggle('active');
-};
-
-// function activeLink() {
-//     partners_tab_wrap.forEach((item) => 
-//     item.classList.remove('active'));
-//     this.classList.add('active');
-// }
-
-// partners_tab_wrap.forEach((item) =>
-// item.addEventListener('click', activeLink));
+$(document).ready(function(){
+  $('#menu li').on('mouseover focusin', function(){
+    $(this).children('ul').stop().fadeIn(0);
+  });
+  $('#menu li').on('mouseleave', function(){
+    $(this).children('ul').stop().fadeOut(0);
+  });
+  $('#menu').on('mouseover focusin', function(){
+    $('.bg_gnb').stop().fadeIn(0);
+    $('#gnb').addClass('mouse');
+  });
+  $('#menu').on('mouseleave focusout', function(){
+      $('.bg_gnb').stop().fadeOut(0);
+      $('#gnb').removeClass('mouse');
+  }); 
+  $('#menu > li').on('focusout', function(){
+    $(this).children('ul').stop().fadeOut(0);
+  });
+});
