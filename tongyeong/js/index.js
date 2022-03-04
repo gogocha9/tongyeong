474bad4122b5e428227ff6043dd48ccbab5f2721
YoungHyun Cha <gogocha9@naver.com>
2022-03-04 18:00:30 +0900

    메인 화면 수정

--- tongyeong/js/index.js
+++ tongyeong/js/index.js
@@ -1,26 +1,26 @@
 "use strict";
 
-let menu_sm = document.querySelector('#menu-sm');
-let gnb = document.querySelector('#gnb');
-let navigation = document.querySelector('#navigation');
-let body = document.querySelector('body');
-let toTop = document.querySelector('#toTop');
+let menu_sm = document.querySelector("#menu-sm");
+let gnb = document.querySelector("#gnb");
+let navigation = document.querySelector("#navigation");
+let body = document.querySelector("body");
+let toTop = document.querySelector("#toTop");
 
-window.onscroll = function() {
-    if(window.scrollY > 0) {
-        toTop.classList.add('down');
-        gnb.classList.add('down');
-        navigation.classList.add('down');
-    } else if (window.scrollY === 0) {
-        gnb.classList.remove('down');
-        toTop.classList.remove('down');
-        navigation.classList.remove('down');
-    }
-}
+window.onscroll = function () {
+  if (window.scrollY > 0) {
+    toTop.classList.add("down");
+    gnb.classList.add("down");
+    navigation.classList.add("down");
+  } else if (window.scrollY === 0) {
+    gnb.classList.remove("down");
+    toTop.classList.remove("down");
+    navigation.classList.remove("down");
+  }
+};
 
-menu_sm.onclick = function() {
-    gnb.classList.toggle('active');
-    menu_sm.classList.toggle('active');
-    navigation.classList.toggle('active');
-    body.classList.toggle('active');
+menu_sm.onclick = function () {
+  gnb.classList.toggle("active");
+  menu_sm.classList.toggle("active");
+  navigation.classList.toggle("active");
+  body.classList.toggle("active");
 };
